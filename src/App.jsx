b06410f9df3add3f03cef6cbb0ca64d2545b297f
unknown <muktinathrajbanshi786@gmail.com>
2025-08-29 21:39:24 +0545

    JSX (JavaScript XML) The Essential Building Block of React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React</h1>
-    </>
+    <div>
+      <div>
+        <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
+      </div>
+      <h2>Name: Tare Zameen Par</h2>
+      <h3>Rating:8.2</h3>
+      <p>
+        An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.
+      </p>
+    </div>
   );
 };
 
