33f49a91d78a8cdd7624919b9c4fe0424376e420
unknown <muktinathrajbanshi786@gmail.com>
2025-08-30 13:25:08 +0545

    Dynamic Values in JSX: Injecting JavaScript into HTML

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,22 @@ const NetflixSeries = () => {
   const rating = "8.2";
   const summary = 
   "An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.";
+  
+  const returnGenre = () => {
+    const genre = "RomCom";
+    return genre;
+  };
   return (
     <div>
       <div>
         <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
       </div>
       <h2>Name : {name}</h2>
-      <h3>Rating : {rating}</h3>
+      <h3>Rating : {5+3.2}</h3>
       <p>
       Summary : {summary}
       </p>
+      <p>Genre: {returnGenre()}</p>
     </div>
   );
 };
