fbfedcae0c295945c3051d30c92ecd7b71fefadf
unknown <muktinathrajbanshi786@gmail.com>
2025-08-30 13:16:30 +0545

    dynamic values in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,19 @@ const App = () => {
 
 
 const NetflixSeries = () => {
+  const name = "Queen of Tears";
+  const rating = "8.2";
+  const summary = 
+  "An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.";
   return (
     <div>
       <div>
         <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
       </div>
-      <h2>Name: Tare Zameen Par</h2>
-      <h3>Rating:8.2</h3>
+      <h2>Name : {name}</h2>
+      <h3>Rating : {rating}</h3>
       <p>
-        An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.
+      Summary : {summary}
       </p>
     </div>
   );
