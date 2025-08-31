d544c8fbef9ca129caeacc21686ea814cdbef174
unknown <muktinathrajbanshi786@gmail.com>
2025-08-31 09:33:48 +0545

    React Conditional rendering in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,56 @@ const NetflixSeries = () => {
   const summary = 
   "An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.";
   
+  let age = 19;
+
+  // let canWatch = "Not Available";
+  // if(age >= 18) canWatch = "Watch Now";
+
+  const canWatch = () => {
+    if(age >= 18) return "Watch Now";
+    return "Not Available";
+  };
+
   const returnGenre = () => {
     const genre = "RomCom";
     return genre;
   };
-  return (
-    <div>
+
+  // if(age < 18) {
+  //   return (
+  //     <div>
+  //       <div>
+  //         <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
+  //       </div>
+  //       <h2>Name : {name}</h2>
+  //       <h3>Rating : {5+3.2}</h3>
+  //       <p>
+  //       Summary : {summary}
+  //       </p>
+  //       <p>Genre: {returnGenre()}</p>
+  //       <button>Not Available</button>
+  //     </div>
+  //   );
+  // }
+
+   return (
       <div>
-        <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
+        <div>
+          <img src="tare_zameen_par.jpg" alt="web img" width="40%" height="40%" />
+        </div>
+        <h2>Name : {name}</h2>
+        <h3>Rating : {5+3.2}</h3>
+        <p>
+        Summary : {summary}
+        </p>
+        <p>Genre: {returnGenre()}</p>
+        {/* <button> {age >=18 ? "Watch Now" : "Not Available" } </button> */}
+        {/* <button>{canWatch}</button> */}
+        <button>{canWatch()}</button>
+
       </div>
-      <h2>Name : {name}</h2>
-      <h3>Rating : {5+3.2}</h3>
-      <p>
-      Summary : {summary}
-      </p>
-      <p>Genre: {returnGenre()}</p>
-    </div>
-  );
+    );
+
 };
 
 
