1c0c491c04966b24bf73e5c16656b662fe363310
unknown <muktinathrajbanshi786@gmail.com>
2025-09-02 17:59:14 +0545

    React Coding Challenge: Passing JSX as Props in React

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { Practices } from './Practices'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Profile from './components/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Practices /> */}
+    <Profile />
   </StrictMode>,
 )
