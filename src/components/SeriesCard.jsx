5d9f5c85b02c664f12a880ed79218956f8910a73
unknown <muktinathrajbanshi786@gmail.com>
2025-09-04 20:53:11 +0545

    Styled Components in React JS for Clean and Dynamic UI

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,19 +1,42 @@
 
 import React from 'react';
 import styles from "./Netflix.module.css";
+import styled from "styled-components";
 
 export const SeriesCard = ({curElem}) => {
     const { img_url, name, rating, description, cast, genre, watch_url } = curElem;
 
-    const btn_style = {
-        padding:"1.2rem 2.4rem",
-        border: "none",
-        fontSize: "1.6rem",
-        backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f" }`,
-        color:"var(--bg-color)",
-        fontWeight: "bold",
-        cursor: "pointer"
-    }
+    // const btn_style = { }
+    // const Button_btn = styled.button (
+    //     {
+    //     padding:"1.2rem 2.4rem",
+    //     border: "none",
+    //     fontSize: "1.6rem",
+    //     backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f" }`,
+    //     color:"var(--bg-color)",
+    //     fontWeight: "bold",
+    //     cursor: "pointer"
+    //     }
+    // )    
+
+
+    const Button_btn = styled.button`
+        padding:1.2rem 2.4rem;
+        border: none;
+        font-size: 1.6rem;
+        background-color: ${(props) => 
+        props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+        color:var(--btn-color);
+        font-weight: bold;
+        cursor: pointer;
+    `; 
+
+    const Rating = styled.h3`
+        font-size: 1.6rem;
+        color: #7dcea0;
+        text-transform: capitalize; 
+    `;
+        
 
     const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
 
@@ -24,15 +47,16 @@ export const SeriesCard = ({curElem}) => {
         </div>
         <div className={styles["card-content"]}>
         <h2>Name: {name} </h2>
-        <h3>Rating: <span className={`${styles.rating} ${ratingClass}`}>
+        <Rating>Rating: <span className={`${styles.rating} ${ratingClass}`}>
          {rating}
           </span> 
-          </h3>
+          </Rating>
         <p>Summary: {description} </p>
         <p>Genre: {genre.join(", ")}</p>
         <p>Cast: {cast.join(", ")}</p>
         <a href={watch_url} target="_blank">
-            <button style={btn_style}>Watch Now</button>
+            {/* <button style={btn_style}>Watch Now</button> */}
+            <Button_btn rating={rating}>Watch Now</Button_btn>
         </a>
         </div>
     </li>
