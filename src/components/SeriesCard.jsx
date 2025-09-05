080146e36c13f3f1b8cf966e7a9f6f324eaf084e
unknown <muktinathrajbanshi786@gmail.com>
2025-09-05 14:01:19 +0545

    Tailwind CSS in React: Installing and Using Tailwind CSS to Style Your React App

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -45,13 +45,14 @@ export const SeriesCard = ({curElem}) => {
         <div>
             <img src={img_url} alt={name} width="40%" height="40%" />
         </div>
-        <div className={styles["card-content"]}>
+        {/* <div className={styles["card-content"]}> */}
+        <div className="flex flex-col gap-6 py-[3.2rem] px-[1.2rem]">
         <h2>Name: {name} </h2>
         <Rating>Rating: <span className={`${styles.rating} ${ratingClass}`}>
          {rating}
           </span> 
           </Rating>
-        <p>Summary: {description} </p>
+        <p className="text-3xl font-bold underline text-cyan-300">Summary: {description} </p>
         <p>Genre: {genre.join(", ")}</p>
         <p>Cast: {cast.join(", ")}</p>
         <a href={watch_url} target="_blank">
