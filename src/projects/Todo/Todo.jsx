661783fb83b33bad440b5c97bdc50c630cb1d1d9
unknown <muktinathrajbanshi786@gmail.com>
2025-09-15 14:19:26 +0545

    wow third one component is added for date and time and used props

--- src/projects/Todo/Todo.jsx
+++ src/projects/Todo/Todo.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "./Todo.css";
-import { MdCheck, MdDeleteForever } from "react-icons/md";
 import { TodoForm } from './TodoForm';
 import { TodoList } from './TodoList';
+import { TodoDate } from './TodoDate';
 
 
 export const Todo = () => {
     
     const [task, setTask] = useState([]);
-    const [dateTime, setDateTime] = useState("");
+    
 
    
 
@@ -35,24 +35,14 @@ export const Todo = () => {
 
     //todo Date and Time
 
-    useEffect(() => {
-    const interval = setInterval(() => {
-        const now = new Date();
-        const formattedDate = now.toLocaleDateString();
-        const formattedTime = now.toLocaleTimeString();
-       setDateTime(`${formattedDate} - ${formattedTime}`);
-    }, 1000);
-
-    return () => clearInterval(interval);
-
-    }, []);
+    <TodoDate />
 
   return (
     <>
       <section className="todo-container">
         <header>
             <h1>Todo List</h1>
-            <h2 className="date-time">{dateTime}</h2>
+          
         </header>
         <TodoForm onAddTodo={handleFormSubmit} />
         <section className="myUnordList">
@@ -60,7 +50,11 @@ export const Todo = () => {
                 {
                     task.map((curTask, index) => {
                         return (
-                            <TodoList key={index} data={curTask} onHandleDeleteTodo={handleDeleteTodo} />
+                            <TodoList 
+                            key={index} 
+                            data={curTask} 
+                            onHandleDeleteTodo={handleDeleteTodo} 
+                            />
                         );
                     })
                 }
