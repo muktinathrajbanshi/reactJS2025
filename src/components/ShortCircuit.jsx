95c9707cc2c024bc8ac3bce5c7110993a3b81b17
unknown <muktinathrajbanshi786@gmail.com>
2025-09-17 20:41:32 +0545

    Or operator is done for ShortCircuit Evaluation

--- src/components/ShortCircuit.jsx
+++ src/components/ShortCircuit.jsx
@@ -4,6 +4,7 @@ import "./ShortCircuit.css";
 export const ShortCircuitExample = () => {
 
   const[isLoggedIn, setIsLoggedIn] = useState(true);
+  const[user, setUser] = useState("");
 
   return (
     <>
@@ -16,9 +17,12 @@ export const ShortCircuitExample = () => {
        { isLoggedIn && <p>You are logged in!</p> }
 
         {/* Another example of short circuit evaluation  */}
+
+        {user ? `Hello ${user}` : "You are logged in!"}
+
         <div className="grid-three-cols">
             <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Toggle Login State</button>
-            <button>Set User</button>
+            <button onClick={() => setUser("Muktinath Rajbanshi")}>Set User</button>
             <button>Clear User</button>
         </div>
         </div>
