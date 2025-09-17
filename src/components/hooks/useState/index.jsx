d58bc2c6c3e35894d32cb9df36b8cdd86c4e125e
unknown <muktinathrajbanshi786@gmail.com>
2025-09-17 21:50:08 +0545

    How to Use the useState Hook in React

--- src/components/hooks/useState/index.jsx
+++ src/components/hooks/useState/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import "../Hooks.css";
 
 export const Counter = () => {
+
+const [count, setCount] = useState(0)
+
+// const handleIncrement = () => {
+//     setCount(count + 1);
+//     console.log("inner", count);
+// }
+    
+// console.log("outer",count);
+
   return (
     <>
       <div className="container state-container" style={{textAlign: "center"}}>
         <h1>useState Hook!</h1>
         <br />
-        <p>Count</p>
-        <button className="state-button">Increment</button>
+        <p>{count}</p>
+        <button className="state-button" onClick={() => setCount(count + 1) }>Increment</button>
       </div>
     </>
   );
