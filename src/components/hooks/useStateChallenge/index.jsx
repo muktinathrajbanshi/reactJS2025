91b03c5a13e86a9142a7e435a909144078f036dd
unknown <muktinathrajbanshi786@gmail.com>
2025-09-18 21:56:37 +0545

    React useState Hook Challenge: Advanced Counter App with Disable, Limits, Steps Counts & More

--- src/components/hooks/useStateChallenge/index.jsx
+++ src/components/hooks/useStateChallenge/index.jsx
@@ -4,16 +4,14 @@ import "./style.css";
 export const UseStateChallenge = () => {
 
     const [count, setCount] = useState(0);
+    const [step, setStep] = useState(0);
 
    const handleIncrement = () => {
-    setCount(count + 1);
+    setCount(count + step);
    };
 
    const handleDecrement = () => {
-
-    if(count >= 0) {
-        setCount(count -1);
-    }
+    setCount(count - step);
    };
 
    const handleReset = () => {
@@ -26,11 +24,17 @@ export const UseStateChallenge = () => {
       <div className="element-controller">
             <h1>useState Challenge</h1>
             <p>Count: <span className="color-value">{count}</span></p>
-            <p>Step:<input type="text" className="input-field" /></p>
+            <p>Step: 
+            <input 
+            type="number"
+            value={step}
+            onChange={(e) =>setStep(Number(e.target.value))}
+            className="input-field" 
+            /></p>
 
             <div className="grid-three-cols">
-                <button onClick={handleIncrement}>Increment</button>
-                <button onClick={handleDecrement}>Decrement</button>
+                <button onClick={handleIncrement} disabled={count >= 100}>Increment</button>
+                <button onClick={handleDecrement} disabled={count <= 0}>Decrement</button>
                 <button onClick={handleReset}>Reset</button>
             </div>
         </div>
