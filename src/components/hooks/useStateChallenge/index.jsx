57b68a2233254630de1a7357f461d4034b2dc96a
unknown <muktinathrajbanshi786@gmail.com>
2025-09-18 20:58:32 +0545

    increment value functionality is done

--- src/components/hooks/useStateChallenge/index.jsx
+++ src/components/hooks/useStateChallenge/index.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
 export const UseStateChallenge = () => {
+
+    const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="container">
-      <div className="element-controller center-element">
+      <div className="element-controller">
             <h1>useState Challenge</h1>
-            <p>Count:</p>
-            <p>Step:</p>
+            <p>Count: <span className="color-value">{count}</span></p>
+            <p>Step:<input type="text" className="input-field" /></p>
 
             <div className="grid-three-cols">
-                <button>Increment</button>
+                <button onClick={() => setCount(count + 1)}>Increment</button>
                 <button>Decrement</button>
                 <button>Reset</button>
             </div>
