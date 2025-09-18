73396ec440042c09936ca349b4f335220d94ff01
unknown <muktinathrajbanshi786@gmail.com>
2025-09-18 19:02:38 +0545

    my design is just completed for challenged

--- src/components/hooks/useStateChallenge/index.jsx
+++ src/components/hooks/useStateChallenge/index.jsx
@@ -5,14 +5,16 @@ export const UseStateChallenge = () => {
   return (
     <>
       <div className="container">
-        <h1>useState Challenge</h1>
-        <p>Count</p>
-        <p>Step</p>
+      <div className="element-controller center-element">
+            <h1>useState Challenge</h1>
+            <p>Count:</p>
+            <p>Step:</p>
 
-        <div className="grid-three-cols">
-            <button>Increment</button>
-            <button>Decrement</button>
-            <button>Reset</button>
+            <div className="grid-three-cols">
+                <button>Increment</button>
+                <button>Decrement</button>
+                <button>Reset</button>
+            </div>
         </div>
       </div>
     </>
