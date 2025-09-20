1301a2a80df830398681c2624a3e0d0919df56c0
unknown <muktinathrajbanshi786@gmail.com>
2025-09-20 23:26:35 +0545

    from one method contact form is submitted

--- src/components/hooks/useState/ContactForm.jsx
+++ src/components/hooks/useState/ContactForm.jsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import "./index.css";
 
 export const ContactForm = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [message, setMessage] = useState("");
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const contactData = {
+        username,
+        password,
+        message,
+    };
+    console.log(contactData);
+    
+}
 
   return (
     <>
       <div className="container">
         <div className="card">
             <h1>Contact Form</h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <label htmlFor="username">Username</label>
                 <input 
                 type="text" 
                 name="username" 
                 required 
                 autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                  />
 
                 <label htmlFor="password">Password</label>
@@ -22,6 +40,8 @@ export const ContactForm = () => {
                 name="password" 
                 required 
                 autoComplete="off" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <label htmlFor="message">Message</label>
@@ -30,6 +50,8 @@ export const ContactForm = () => {
                 required
                 autoComplete="off"
                 rows="6"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 />
                 <button type="submit">Send Message</button>
             </form>
