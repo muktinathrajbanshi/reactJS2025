58515d7f59e8f7d160454feef180405bb757c165
unknown <muktinathrajbanshi786@gmail.com>
2025-09-20 22:56:54 +0545

    passing data to form

--- src/components/hooks/useState/LoginForm.jsx
+++ src/components/hooks/useState/LoginForm.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./index.css";
 
 export const LoginForm = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+
   return (
     <>
       <div className="container">
@@ -9,10 +14,24 @@ export const LoginForm = () => {
             <h1>Login Form</h1>
             <form>
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" required autoComplete="off" />
+                <input 
+                type="text" 
+                name="username" 
+                required 
+                autoComplete="off"
+                value={usename}
+                onChange={(e) => setUsername(e.target.value)}
+                 />
 
                 <label htmlFor="password">Password</label>
-                <input type="text" name="password" required autoComplete="off" />
+                <input 
+                type="text" 
+                name="password" 
+                required 
+                autoComplete="off" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
 
                 <button type="submit">Login</button>
             </form>
