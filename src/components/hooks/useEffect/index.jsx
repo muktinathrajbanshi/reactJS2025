0e52087277e45a1bffc0c0572c6ed713d0232ee4
unknown <muktinathrajbanshi786@gmail.com>
2025-09-21 15:59:00 +0545

    useEffect Hook with Dependency Array in React JS: Learn with 3 Practical Examples

--- src/components/hooks/useEffect/index.jsx
+++ src/components/hooks/useEffect/index.jsx
@@ -3,21 +3,22 @@ import "./index.css";
 
 export const ReactUseEffect = () => {
 
-    const [count, setCount] = useState(0);
+    const [date, setDate] = useState(0);
+
 
 
     useEffect(() => {
-        console.log("count value:", count);
-        
-    }, [count]);
+        setInterval(() => {
+        const updatedDate = new Date();
+        setDate(updatedDate.toLocaleTimeString());
+    }, 1000);
+    }, []);
 
   return (
     <>
       <div className="container">
         <div className="effect-container">
-            <h1>useEffect Hook</h1>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <h1>Date : {date }</h1>
         </div>
       </div>
     </>
