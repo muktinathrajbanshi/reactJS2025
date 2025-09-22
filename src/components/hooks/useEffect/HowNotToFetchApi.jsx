93c018558b394abb95a9338566c7800961b874fb
unknown <muktinathrajbanshi786@gmail.com>
2025-09-22 18:19:06 +0545

    How NOT to Fetch API Data in React: Avoid Infinite Requests

--- src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 import "./Pokemon.css";
 
 export const HowNotToFetchApi = () => {
+
+    const [apiData, setApiData] = useState([]);
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) =>{
+            // setApiData(data);
+        })
+    .catch((error) => console.log(error));
+    
+    
+    
   return (
-    <>
+    
       <div className="container effect-container">
-        <ul>data:</ul>
+        <ul>data:
+        {apiData.map((curData) => {
+            return <li key={curData.id}> {curData.title} </li>
+        })}
+        </ul>
       </div>
-    </>
+    
   );
 };
