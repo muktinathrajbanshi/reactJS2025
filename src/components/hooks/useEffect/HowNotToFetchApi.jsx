105e8e8651d4790bcb2f0b4c425592ece6bb5df9
unknown <muktinathrajbanshi786@gmail.com>
2025-09-22 22:49:51 +0545

    handling loading is done

--- src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -4,16 +4,23 @@ import "./Pokemon.css";
 export const HowNotToFetchApi = () => {
 
     const [pokemon, setPokemon] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
-    const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
+    const API = "https://pokeapi.co/api/v2/pokemons/pikachu";
 
     const fetchPokemon = () => {
         fetch(API)
         .then((res) => res.json())
         .then((data) =>{
             setPokemon(data);
+            setLoading(false);
             })
-        .catch((error) => console.log(error));
+        .catch((error) => {
+            console.log(error); 
+            setError(error);
+            setLoading(false);
+        });
     };
 
     useEffect(() => {
@@ -22,11 +29,19 @@ export const HowNotToFetchApi = () => {
 
     console.log(pokemon);
 
-    if(!pokemon) return(
+    if(loading) 
+        return(
         <div>
         <h1>Loading....</h1>
         </div>
     );
+
+    if (error) 
+    return (
+        <div>
+         <h1>Error: {error.message}</h1>
+        </div>
+    )
 
 // if(pokemon) {
   return (
