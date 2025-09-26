ab1641ba782eefef2979b0b89c0be0f5fa07146c
unknown <muktinathrajbanshi786@gmail.com>
2025-09-26 17:13:48 +0545

    useRef Hook in React to manage DOM elements and State easily

--- src/components/hooks/useRefs/index.jsx
+++ src/components/hooks/useRefs/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { useRef } from 'react';
 import "./index.css";
 
 export const UseRef = () => {
+
+    // const username = document.getElementById("username");
+    // const password = document.getElementById("password");
+
+    const username = useRef(null);
+    const password = useRef(null);
+
+    // console.log(username);
+    
+    
+    
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(username.current.value, password.current.value);
+    };
+
   return (
     <>
     <div className="container">
-      <form className="form-design">
+      <form onSubmit={handleFormSubmit} className="form-design">
         <label htmlFor="Username">Username</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" ref={username} />
         <label htmlFor="Password">Password</label>
-        <input type="text" id="password" />
+        <input type="text" id="password" ref={password} />
         <div className="button-control">
         <button>Submit</button>
         </div>
