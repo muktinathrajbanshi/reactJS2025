e9a07f04f4c17d6b6e30230819d1c414f424429e
unknown <muktinathrajbanshi786@gmail.com>
2025-09-27 21:56:53 +0545

    useId Hook in React 19: Simplify Your Form Handling with Unique IDs

--- src/components/hooks/UseID/index.jsx
+++ src/components/hooks/UseID/index.jsx
@@ -43,13 +43,13 @@ export const UseId = () => {
             <h1>UseId</h1>
         </div>
             <label htmlFor={id + "usernameId"}>Username:</label>
-            <input type="text" id={usernameId} name="name" />
+            <input type="text" id={id + "usernameId"} name="name" />
 
-            <label htmlFor={passwordId}>Password:</label>
-            <input type="password" id={passwordId} name="password" />
+            <label htmlFor={id + "passwordId"}>Password:</label>
+            <input type="password" id={id + "passwordId"} name="password" />
 
-            <label htmlFor={emailId}>Email:</label>
-            <input type="email" id={emailId} name="email" />
+            <label htmlFor={id + "emailId"}>Email:</label>
+            <input type="email" id={id + "emailId"} name="email" />
         <div className="btn">
             <button type="submit">Submit</button>
         </div>
