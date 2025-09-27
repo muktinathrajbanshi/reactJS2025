99a9c66e0d7e55ff520140d0f15991e7465e568c
unknown <muktinathrajbanshi786@gmail.com>
2025-09-27 21:47:21 +0545

    previous code does not follow reusable code now from this i am making happens

--- src/components/hooks/UseID/index.jsx
+++ src/components/hooks/UseID/index.jsx
@@ -3,9 +3,37 @@ import "./index.css";
 
 export const UseId = () => {
 
-    const usernameId = useId();
-    const emailId = useId();
-    const passwordId = useId();
+//     const usernameId = useId();
+//     const emailId = useId();
+//     const passwordId = useId();
+
+//   return (
+//     <>
+//     <div className="container">
+//       <form className="form-data-management">
+//         <div className="heading">
+//             <h1>UseId</h1>
+//         </div>
+//             <label htmlFor={usernameId}>Username:</label>
+//             <input type="text" id={usernameId} name="name" />
+
+//             <label htmlFor={passwordId}>Password:</label>
+//             <input type="password" id={passwordId} name="password" />
+
+//             <label htmlFor={emailId}>Email:</label>
+//             <input type="email" id={emailId} name="email" />
+//         <div className="btn">
+//             <button type="submit">Submit</button>
+//         </div>
+//       </form>
+//       </div>
+//     </>
+//   );
+// };
+
+// this lets you avoid calling useId for every single element that needs a unique ID.
+
+    const id = useId();
 
   return (
     <>
@@ -14,7 +42,7 @@ export const UseId = () => {
         <div className="heading">
             <h1>UseId</h1>
         </div>
-            <label htmlFor={usernameId}>Username:</label>
+            <label htmlFor={id + "usernameId"}>Username:</label>
             <input type="text" id={usernameId} name="name" />
 
             <label htmlFor={passwordId}>Password:</label>
