11f0657ea803d626023e4ab6a950cf3f6cc19ba8
unknown <muktinathrajbanshi786@gmail.com>
2025-09-28 19:44:14 +0545

    finally structure is created

--- src/components/PropDrilling.jsx
+++ src/components/PropDrilling.jsx
@@ -6,6 +6,34 @@ export const PropDrilling = () => {
   return (
     <section className="data-management">
     <h1>Component A</h1>
+    <ChildComponent data="React JS" />
     </section>
   );
+};
+
+
+const ChildComponent = (props) => {
+    return (
+        <>
+            <h1>Hello, I am component B</h1>
+            <GrandChildComponent data={props.data} />
+        </>
+    );
+};
+
+const GrandChildComponent = (props) => {
+    return (
+        <>
+            <h1>Hello, I am component C</h1>
+            <GrandGrandChildComponent data={props.data} />
+        </>
+    );
+};
+
+const GrandGrandChildComponent = (props) => {
+    return (
+        <>
+            <h1>Hello, I love {props.data}</h1>
+        </>
+    );
 };
