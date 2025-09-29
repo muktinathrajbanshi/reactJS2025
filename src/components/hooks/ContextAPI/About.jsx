2d62391b29078a18e3ffa688c24440748e63a5b8
unknown <muktinathrajbanshi786@gmail.com>
2025-09-29 18:05:39 +0545

    wow css is added now it's look perfect

--- src/components/hooks/ContextAPI/About.jsx
+++ src/components/hooks/ContextAPI/About.jsx
@@ -1,10 +1,15 @@
-import { useContext } from "react";
-import { BioContext } from ".";
+
+import { useBioContext } from ".";
 
 export const About = () => {
 
-    const {myName, myAge} = useContext(BioContext);
+    const {myName, myAge} = useBioContext();
 
-    return <h1>Hello Context API (About). My name is {myName}. I am {myAge} years old.</h1>;
+    return (
+        <section>
+            <h1>Hello Context API (About). My name is {myName}. I am {myAge} years old.</h1>;
+        </section>
+    );
+    
 };
 
--- src/components/hooks/ContextAPI/Home.jsx
+++ src/components/hooks/ContextAPI/Home.jsx
@@ -1,11 +1,14 @@
 import { useBioContext } from ".";
+import "./contextapi.css";
 
 export const Home = () => {
 
     const {myName, myAge} = useBioContext();
 
     return (
+        <section>
         <h1>Hello Context API. My name is {myName}. I am {myAge} years old.</h1>
+        </section>
     ); 
 };
 
