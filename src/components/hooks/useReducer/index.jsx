33a9b18bb775b1e9f0d419e8e7bd56bba702e81f
unknown <muktinathrajbanshi786@gmail.com>
2025-09-30 20:35:14 +0545

    totally modified with reusable code

--- src/components/hooks/useReducer/index.jsx
+++ src/components/hooks/useReducer/index.jsx
@@ -4,29 +4,50 @@ import "./index.css";
 
 export const ReducerComp = () => {
 
+    const initialState = {
+        count: 0,
+    };
+
     const reducer = (state, action) => {
         console.log(state, action);
 
-        if(action.type === "INCREMENT") {
-            return state + 1;
-        }
+        // if(action.type === "INCREMENT") {
+        //     return state + 1;
+        // }
         
-        if(action.type === "DECREMENT") {
-            return state - 1;
+        // if(action.type === "DECREMENT") {
+        //     return state - 1;
+        // }
+
+        // if(action.type === "RESET") {
+        //     return (state = 0);
+        // }
+
+        switch (action.type) {
+            case "INCREMENT":
+                return {count: state.count + 1};
+
+            // case "DECREMENT":
+            //     return state - 1;
+
+            //  case "RESET":
+            //     return (state = 0);
+            // default:
+            //     return state;
         }
+
+
     };
 
     // const [count, setCount] = useState(0);
-    const [count, dispatch] = useReducer(reducer, 0);
-
-
-
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <>
             <div className="container">
-                <h1>{count}</h1>
+                <h1>{state.count}</h1>
                 <button onClick={() => dispatch({type:"INCREMENT"})}>Increment</button>
                 <button onClick={() => dispatch({type:"DECREMENT"})}>Decrement</button>
+                <button onClick={() => dispatch({type:"RESET"})}>Reset</button>
             </div>
         </>
     );
