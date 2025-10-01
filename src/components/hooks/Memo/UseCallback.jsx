107fbf431c9cb111198695c551678569e1ef9843
unknown <muktinathrajbanshi786@gmail.com>
2025-10-01 22:28:21 +0545

    wow now i know the actual functionality of useCallback hook

--- src/components/hooks/Memo/UseCallback.jsx
+++ src/components/hooks/Memo/UseCallback.jsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
+import { memo, useCallback, useState } from 'react';
+import "./memo.css";
 
-// const Button = () => {
-//         console.log(`Rendering button: ${children}`);
+const Button = memo(({ onClick, children }) => {
+        console.log(`Rendering button: ${children}`);
 
-//         return (
-//             <button onClick={onClick}>{children}</button>
-//         );
-//     };
+        return (
+            <button className={`${children === "Increment" ? "green" : "red"}`} onClick={onClick}>
+            {children}
+            </button>
+        );
+    });
 
 export const UseCallback = () => {
 
     const [count, setCount] = useState(0);
 
     //Memoize the increment function
-    const increment = () => {
+    // const increment = () => {};
+
+    const increment = useCallback(() => {
         console.log("increment inside");
         setCount((prevCount) => prevCount + 1);
-    };
+    }, []);
 
     // Memoize the decrement function
-    const decrement = () => setCount((prevCount) => prevCount -1);
+    const decrement = () => {
+        // if(count>0) {
+        //     setCount((prevCount) => prevCount -1)
+        // }
+        setCount((prevCount) => prevCount -1)
+
+    };
 
   return (
     <>
         <div className="container">
             <h1>Count: {count}</h1>
             <Button onClick={increment}>Increment</Button>
-            <Button onClick={decrement}>Increment</Button>
+            <Button onClick={decrement}>Decrement</Button>
         </div>
     </>
   );
