d1318ab0c1f3ac9e3ed93f8b93b3402e8ebc6fa7
unknown <muktinathrajbanshi786@gmail.com>
2025-10-01 19:54:37 +0545

    wow little bit code is written in javascript basic for this project

--- src/components/hooks/Memo/useMemo.jsx
+++ src/components/hooks/Memo/useMemo.jsx
@@ -1,6 +1,21 @@
 import { useState } from 'react';
 import "./memo.css";
 
+const ExpensiveComponent = () => {
+    const sum = () => {
+        console.log("Calculating sum...");
+        let i = 0;
+        for (i = 0; i <= 1000000000; i++) {
+            i = i + 1;
+        }
+        return i; 
+    };
+
+    const total = sum();
+
+    return <p> sum: {total} </p>;
+}
+
 export const MemoParentComponent = () => {
 
     const [count, setCount] = useState(0);
@@ -8,6 +23,7 @@ export const MemoParentComponent = () => {
   return (
     <>
         <div className="container">
+        <ExpensiveComponent />
         <button onClick={() => setCount(count + 1)}>Re-render Parent</button>
         <p>Parent re-renders: {count}</p>
         </div>
